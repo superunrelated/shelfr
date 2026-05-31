aa3169f1b762cd6b567be182b668279c17b51368
Knut Skaala <knut@77k.cc>
2026-06-01 07:19:43 +1200

    android fix

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -140,6 +140,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Suspense>
   );
